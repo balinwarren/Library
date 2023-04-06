5c2aa7a1a5e782937a731f23df2afdfa7983806e
Balin Warren <balin.warren@gmail.com>
2023-04-06 10:32:24 -0700

    Added button functionality to Add Book button

--- app.js
+++ app.js
@@ -10,3 +10,24 @@ function Book(title, author, pages, haveRead) {
 function addBookToLibrary() {
   
 }
+
+//UI
+
+const addBookBtn = document.getElementById('add-book');
+const addBookForm = document.getElementById('add-book-form');
+const addBookModal = document.getElementById('addBookModal');
+const overlay = document.getElementById('overlay');
+
+const openBookForm = () => {
+    addBookForm.reset();
+    addBookModal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+const closeBookForm = () => {
+    addBookModal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+addBookBtn.onclick = openBookForm;
+overlay.onclick = closeBookForm;

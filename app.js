d6dc7f279b965538f516755c5d1c9d298ec35492
Balin Warren <balin.warren@gmail.com>
2023-04-10 12:58:37 -0700

    Added create card function to built out card element creation when books are added to library

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+const cardContainer = document.getElementById('cardContainer');
 
 function Book(title, author, pages, haveRead) {
   this.title = title;
@@ -12,12 +13,54 @@ const addBookToLibrary = (e) => {
   let title = document.getElementById('title');
   let author = document.getElementById('author');
   let pages = document.getElementById('pages');
-  let isRead = document.getElementById('is-read');
+  let haveRead = document.getElementById('is-read');
 
-  let book = new Book(title.value, author.value, pages.value, isRead.checked);
+  let book = new Book(title.value, author.value, pages.value, haveRead.checked);
   myLibrary.push(book);
   
   closeBookForm();
+  createCards();
+}
+
+const createCards = () => {
+    while (cardContainer.firstChild) {
+        cardContainer.removeChild(cardContainer.lastChild);
+    }
+
+    for(let i = 0; i < myLibrary.length; i++) {
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+        bookCard.dataset.index = i;
+        
+        let title = document.createElement('h3');
+        title.innerHTML = '"' + myLibrary[i].title + '"';
+
+        let author = document.createElement('h3');
+        author.innerHTML = myLibrary[i].author;
+
+        let pages = document.createElement('h3');
+        pages.innerHTML = myLibrary[i].pages;
+
+        let read = document.createElement('h3');
+        if (myLibrary[i].haveRead == true) {
+            read.innerHTML = 'Have Read';
+        } else if (myLibrary[i].haveRead == false) {
+            read.innerHTML = 'Not Read';
+        }
+
+        let remove = document.createElement('button');
+        remove.innerHTML = "Remove";
+        remove.classList.add('btn');
+        remove.classList.add('remove-button');
+
+        bookCard.appendChild(title);
+        bookCard.appendChild(author);
+        bookCard.appendChild(pages);
+        bookCard.appendChild(read);
+        bookCard.appendChild(remove);
+
+        cardContainer.appendChild(bookCard);
+    }
 }
 
 //UI
